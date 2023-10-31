09600b1daa9e53792653fa010a2cf4e031444869
diya <diyajcyriac@hotmail.com>
2023-10-31 09:39:47 +0530

    added Authorisation

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const jwt = require("jsonwebtoken");
 dotenv.config();
+
+// const secretKey = process.env.JWT_KEY;
 
 const app = express();
 app.use(express.json());
@@ -48,6 +51,53 @@ app.use(
 
 app.listen(5000, () => {
   console.log("Listening...");
+});
+
+// Login Credentials
+
+// app.use("/login", (req, res) => {
+//   res.send({
+//     token: "test123",
+//   });
+// });
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.username;
+    const password = req.body.password;
+    console.log(email);
+
+    if (!(email && password)) {
+      return res.status(400).send("All input is required");
+    }
+
+    const query =
+      "SELECT * FROM users WHERE email = $1 AND password = crypt($2, password)";
+    const values = [email, password];
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 1) {
+      const user = result.rows[0];
+
+      const token = jwt.sign(
+        { user_id: user.user_id, email },
+        process.env.TOKEN_KEY,
+        { expiresIn: "2h" }
+      );
+
+      const updateUserQuery = "UPDATE users SET token = $1 WHERE user_id = $2";
+      const updateUserValues = [token, user.user_id];
+      await pool.query(updateUserQuery, updateUserValues);
+
+      user.token = token;
+      return res.status(200).json({"token":token});
+    }
+
+    return res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 // POST endpoint to insert data
@@ -368,4 +418,102 @@ app.get("/history/header/", async (req, res) => {
   const result = await getProjectDataUsingId(req);
   console.log(result, "herre111");
   res.status(200).send(result);
+});
+
+// POST endpoint to insert data into commit_history table
+
+/**
+ * @swagger
+ * /history/insert:
+ *   post:
+ *     summary: Insert commit history
+ *     description: Insert commit history data into the database.
+ *     tags:
+ *       - History
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               branch_name:
+ *                 type: string
+ *               commit_date:
+ *                 type: string
+ *                 format: date
+ *               commit_id:
+ *                 type: string
+ *               no_of_deletion:
+ *                 type: integer
+ *               no_of_addition:
+ *                 type: integer
+ *               project_id:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Success message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *       400:
+ *         description: Error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 err:
+ *                   type: string
+ *       243:
+ *         description: Project ID does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+
+app.post("/history/insert", (req, res) => {
+  const username = req.body.username;
+  const branch_name = req.body.branch_name;
+  const commit_date = req.body.commit_date;
+  const commit_id = req.body.commit_id;
+  const no_of_deletion = req.body.no_of_deletion;
+  const no_of_addition = req.body.no_of_addition;
+  const project_id = req.body.project_id;
+
+  pool.query(
+    `INSERT INTO commit_history (project, user_name, branch_name, commit_date, commit_id, num_additions, num_deletions)
+    VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+    [
+      project_id,
+      username,
+      branch_name,
+      commit_date,
+      commit_id,
+      no_of_addition,
+      no_of_deletion,
+    ],
+    (err, result) => {
+      if (err) {
+        if (err.constraint === "fk_project") {
+          return res.status(243).json({ error: "project_id does not exist" });
+        } else {
+          res.send({ err: err });
+        }
+      } else {
+        res.send({ message: "added", data: result.rows[0] });
+      }
+    }
+  );
 });
